5627d5dde1868481bf9e6432e9a638c50b73405f
Vatsal Jain <vatsalpjain@gmail.com>
2024-12-27 20:37:10 +0530

    Add this initial file for second

---
+++ crazysecond.java
@@ -0,0 +1,7 @@
+package Secondproject;
+
+public class crazysecond {  // class name should be same as file name
+    public static void main(String[] args) {
+        System.out.println("This is my new repository");
+    }
+}
